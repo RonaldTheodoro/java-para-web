7530dd29e8f4fea25be418822f8cf2e40be9608d
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-02-12 20:21:06 -0200

    implementado o bean Contato

--- exemplosjdbc/src/br/com/javaparaweb/capitulo03/crudjdbc/Contato.java
+++ exemplosjdbc/src/br/com/javaparaweb/capitulo03/crudjdbc/Contato.java
@@ -1,5 +1,61 @@
 package br.com.javaparaweb.capitulo03.crudjdbc;
 
+import java.sql.Date;
+
 public class Contato {
+    private Integer codigo;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Date dt_cad;
+    private String obs;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDt_cad() {
+        return dt_cad;
+    }
+
+    public void setDt_cad(Date dt_cad) {
+        this.dt_cad = dt_cad;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
 
 }
