f39de68bb757f966cfea19aa3a1138b4326502e9
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-02-12 00:46:46 -0200

    initial commit

---
+++ teste/src/main/java/br/com/javaparaweb/teste/web/UsuarioBean.java
@@ -0,0 +1,64 @@
+package br.com.javaparaweb.teste.web;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class UsuarioBean {
+    private String nome;
+    private String email;
+    private String senha;
+    private String confirmarSenha;
+
+    public String novo() {
+        return "usuario";
+    }
+
+    public String salvar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (!this.senha.equalsIgnoreCase(this.confirmarSenha)) {
+            context.addMessage(
+                null,
+                new FacesMessage(
+                    FacesMessage.SEVERITY_ERROR,
+                    "Senha confirmada incorretamente",
+                    ""
+                )
+            );
+            return "usuario";
+        }
+        return "mostrausuario";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getConfirmarSenha() {
+        return confirmarSenha;
+    }
+
+    public void setConfirmarSenha(String confirmarSenha) {
+        this.confirmarSenha = confirmarSenha;
+    }
+
+}
