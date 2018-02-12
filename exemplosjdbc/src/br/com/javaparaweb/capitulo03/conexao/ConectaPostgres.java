1d39593962d295cfe72254a0a9637df092bae396
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-02-12 19:46:46 -0200

    remove tabs de ConectaPostgres

--- exemplosjdbc/src/br/com/javaparaweb/capitulo03/conexao/ConectaPostgres.java
+++ exemplosjdbc/src/br/com/javaparaweb/capitulo03/conexao/ConectaPostgres.java
@@ -7,18 +7,19 @@ import java.sql.SQLException;
 public class ConectaPostgres {
 
     public static void main(String[] args) {
-		Connection conexao = null;
-		try {
-			String url = "jdbc:postgresql://172.17.0.2:5432/app";
-			String usuario = "postgres";
-			String senha = "asdf1234";
-			conexao = DriverManager.getConnection(url, usuario, senha);
-			System.out.println("Conectou!");
-			conexao.close();
-		} catch (SQLException e) {
-			System.out.println(
-				"Ocorreu um erro ao criar a conexão. Erro: " + e.getMessage()
-			);
-		}
-	}
+        Connection conexao = null;
+        try {
+            String url = "jdbc:postgresql://172.17.0.2:5432/app";
+            String usuario = "postgres";
+            String senha = "asdf1234";
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conectou!");
+            conexao.close();
+        } catch (SQLException e) {
+            System.out.println(
+                "Ocorreu um erro ao criar a conexão. Erro: " + e.getMessage()
+            );
+        }
+    }
+
 }
