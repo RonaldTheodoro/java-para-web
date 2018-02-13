f81a531115b37b8e01cf912e510a091038c2c0d9
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-02-13 18:34:43 -0200

    Adicionado o projeto comercio

---
+++ comercio/src/main/java/br/com/javaparaweb/comercio/util/HibernateUtil.java
@@ -0,0 +1,34 @@
+package br.com.javaparaweb.comercio.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        SessionFactory sessao = null;
+        try {
+            Configuration cfg = new Configuration();
+            cfg.configure("hibernate.cfg.xml");
+            StandardServiceRegistryBuilder registradorServico;
+            registradorServico = new StandardServiceRegistryBuilder();
+            registradorServico.applySettings(cfg.getProperties());
+            StandardServiceRegistry servico = registradorServico.build();
+            sessao = cfg.buildSessionFactory(servico);
+        } catch (Throwable e) {
+            System.out.println(
+                "Criação inicial do objeto SessionFactory falhou. Erro: "
+                + e.getMessage()
+            );
+        }
+        return sessao;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
