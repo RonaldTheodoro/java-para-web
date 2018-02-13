5b7877619437a762fdc009621ad62980871c35ce
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-02-13 19:34:49 -0200

    implementada a entidade Cliente

--- comercio/src/main/java/br/com/javaparaweb/comercio/entidades/Cliente.java
+++ comercio/src/main/java/br/com/javaparaweb/comercio/entidades/Cliente.java
@@ -1,5 +1,123 @@
 package br.com.javaparaweb.comercio.entidades;
 
-public class Cliente {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 7329206636876103979L;
+
+    @Id
+    @GeneratedValue
+    @Column(name="code_cliente")
+    private Integer cliente;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn(name="cod_cliente")
+    private Endereco endereco;
+
+    @OneToMany(mappedBy="cliente", fetch=FetchType.LAZY)
+    private List<Pedido> pedidos;
+
+    @Column(length=45)
+    private String nome;
+
+    public Cliente(Endereco endereco, List<Pedido> pedidos, String nome) {
+        super();
+        this.endereco = endereco;
+        this.pedidos = pedidos;
+        this.nome = nome;
+    }
+
+    public Integer getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Integer cliente) {
+        this.cliente = cliente;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+        result = prime * result
+            + ((endereco == null) ? 0 : endereco.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((pedidos == null) ? 0 : pedidos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (cliente == null) {
+            if (other.cliente != null)
+                return false;
+        } else if (!cliente.equals(other.cliente))
+            return false;
+        if (endereco == null) {
+            if (other.endereco != null)
+                return false;
+        } else if (!endereco.equals(other.endereco))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (pedidos == null) {
+            if (other.pedidos != null)
+                return false;
+        } else if (!pedidos.equals(other.pedidos))
+            return false;
+        return true;
+    }
 
 }
